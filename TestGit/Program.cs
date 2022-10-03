af4f9683926026ebb5958d4601c2b95096cfb749
BozhijOduvan <garasov2011@yandex.ru>
2022-10-03 21:28:00 +0300

    программа ёлка

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите желаемое количество треугольников: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        string branch = new String('*', j);
+        Console.WriteLine(branch.PadLeft(n + 3) + "*" + branch);
+    }
+}
